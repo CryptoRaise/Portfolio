cb706b5d1ca96d279da6fc89b318e21afc61a63b
Aryan Tiwari <91017115+MrAryan8728@users.noreply.github.com>
2024-04-19 11:59:12 +0530

    fixed error

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -30,7 +30,7 @@ export const Contact = () => {
                 </textarea>
 
                 <button className=' text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>
-                    Let's talk
+                    Let	&apos;s talk
                 </button>
             </form>
         </div>
